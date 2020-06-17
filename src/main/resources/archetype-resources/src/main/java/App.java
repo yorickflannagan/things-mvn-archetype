9eded82078253390970e9af7c9dcfdc0651c5586
Yorick Flanngan <yorick.flanngan@gmail.com>
2020-06-17 18:29:20 -0300

    Skeleton application revised

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -15,41 +15,48 @@ import org.crypthing.things.snmp.ProcessingEvent.ProcessingEventType;
  */
 public class App extends Sandbox
 {
-    private static Logger log = Logger.getLogger(App.class.getName());
+	private static Logger log = Logger.getLogger(App.class.getName());
 
+	@Override
+	public void startup(Properties props) throws ConfigException
+	{
+		// This method is called when application starts
+		// The argument receives configured parameters (see section sandbox)
+		// TODO: overrides only if required 
+		fire(new ProcessingEvent(ProcessingEventType.info, "Thread started."));
+	}
+	@Override
+	public void release()
+	{
+		// This method is called when the application finalizes
+		// TODO: overrides only if required
+		fire(new ProcessingEvent(ProcessingEventType.info, "Thread ended."));
+	}
 
-    @Override
-    public void startup(Properties props) throws ConfigException {
-        /* Inicialização da thread. Recebe as propriedades específicas do projeto */
-        fire(new ProcessingEvent(ProcessingEventType.info, "Thread started."));
-        log.info(props.getProperty("hello"));
-    }
-
-    @Override
-    protected  boolean execute() throws IOException, SQLException
-    {
-        while(isRunning() && hasWork())
-        {
-            try {
-                doSomething();
-                success();
-            } catch (Exception e) {
-                failure();
-            }
-        }
-        quant_work = 10;
-        return true;
-    } 
-
-    private void doSomething() throws Exception {
-        log.fine("Hello World!");
-    }
-
-    long quant_work = 10;
-    private boolean hasWork()
-    {
-        /* TODO:Criar uma condição. */
-        return quant_work-->0;
-    }
-    
+	@Override
+	protected  boolean execute() throws IOException, SQLException
+	{
+		while(isRunning() && hasWork())
+		{
+			if (doYourJob()) success();
+			else failure();
+		}
+		return true;
+	} 
+	private boolean doYourJob()
+	{
+		// TODO: Your unit of work must be implemented here
+		// Return true if it succeeds; otherwise, false. False does not stops working, only signals a failure.
+		// If you want to stop execution, you must throw an exception.
+		// Throw IOException or SQLException only for transient errors. So, the thread will only sleep.
+		// If any other kind of exception is thrown, the thread will die and application stops.
+		log.fine("Hello World!");
+		return true;
+	}
+	private boolean hasWork()
+	{
+		// TODO: Check if there are more units of work to be done.
+		// If you return false, the thread will sleep a little
+		return true;
+	}
 }
